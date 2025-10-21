b7f82889831fd329b8090239525d6431420b072a
corodimas <natdanaipoonpon@scg.com>
2025-10-21 16:15:03 +0700

    Update database settings in settings.py to use environment variables and adjust SSL options for MySQL

--- config/settings.py
+++ config/settings.py
@@ -79,22 +79,19 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME', 'django_db'),
-        'USER': os.getenv('DB_USER', 'root'),
-        'PASSWORD': os.getenv('DB_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '3306'),
-        'OPTIONS': {
-            'ssl': {
-                'ca': None,
-                'cert': None,
-                'key': None,
-            }
-        }
-    }
+  "default": {
+    "ENGINE": "django.db.backends.mysql",
+    "NAME": os.getenv("DB_NAME"),
+    "USER": os.getenv("DB_USER"),
+    "PASSWORD": os.getenv("DB_PASSWORD"),
+    "HOST": os.getenv("DB_HOST"),
+    "PORT": os.getenv("DB_PORT", "3306"),
+    "OPTIONS": {
+      "ssl": {"ssl-mode": "DISABLED"},  # mysqlclient >= 2.1
+    },
+  }
 }
+
 
 
 # Password validation
